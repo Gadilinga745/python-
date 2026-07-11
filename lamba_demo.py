87b93afdbcf68c9e525809136a67da8aff278f4b
Gadik <gadik40270@gmail.com>
2026-07-11 11:57:26 +0530

    Added lambda_demo program

---
+++ lamba_demo.py
@@ -0,0 +1,10 @@
+# def square (num):
+#     return num*num
+# lambda is nothing instead of creating a function
+# in 2 lines we can write it in singel line itself 
+# and hear lambda is used to define ananimous function or unknown function
+fun=lambda num :num*num
+print(fun(5))
+
+add =lambda a,b : a+b
+print(add(3,4))
